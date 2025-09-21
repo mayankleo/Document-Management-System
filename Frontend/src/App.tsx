05018a149a6f0a6e63373ed01955bd14350489b6
Mayank kushwaha <mayankkushwaha226@gmail.com>
2025-09-22 02:30:33 +0530

    init routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,36 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import UploadPage from './pages/UploadPage';
+import SearchPage from './pages/SearchPage';
+import AdminPage from './pages/AdminPage';
+import HomePage from './pages/HomePage';
+import AppLayout from './components/AppLayout';
+import ProfilePage from './pages/ProfilePage';
+
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
+import NotFound from './pages/NotFound';
+
 const App = () => {
   return (
-    <div>Document Management System</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<ProtectedRoute />}> 
+          <Route element={<AppLayout />}> 
+            <Route path="/" element={<HomePage />} />
+            <Route path="/documents" element={<SearchPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route element={<AdminRoute />}> 
+              <Route path="/admin" element={<AdminPage />} />
+              <Route path="/upload" element={<UploadPage />} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
